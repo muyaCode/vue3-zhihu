dcb119bdffe2b69726505cb7de11fc187732abc7
muyi <38781672@qq.com>
2022-08-26 01:27:20 +0800

    feat：添加input组件

---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="validate-input-container pb-3">
+    <input
+      class="form-control"
+      :class="{'is-invalid': inputRef.error}"
+      :value="inputRef.val"
+      @blur="validateInput"
+      @input="updateValue"
+      v-bind="$attrs"
+    >
+    <!-- v-bind="$attrs" 和inheritAttrs: false  配合使用 -->
+    <span v-if="inputRef.error" class="invalid-feedback">{{inputRef.message}}</span>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, PropType, reactive } from 'vue'
+
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+interface RuleProp {
+    type: 'required' | 'email';
+    message: string;
+}
+export type RulesProp = RuleProp[];
+
+export default defineComponent({
+  name: 'ValidateInput',
+  props: {
+    rules: Array as PropType<RulesProp>,
+    modelValue: String
+  },
+  inheritAttrs: false, // 禁用 Attribute 在组件根元素中继承：避免父作用域的不被认作props的特性绑定应用在子组件的根元素上
+  setup(props, context) {
+    const inputRef = reactive({
+        val: props.modelValue || '',
+        error: false,
+        message: ''
+    })
+
+    const updateValue = (e: Event) => {
+        const targetValue = (e.target as HTMLInputElement).value
+        inputRef.val = targetValue
+        // 实现更新modelValue
+        context.emit('update:modelValue', targetValue)
+    }
+
+    const validateInput = () => {
+        if(props.rules) {
+            const allPassed = props.rules.every(rule =>{
+                let passed = true
+                inputRef.message = rule.message
+                switch (rule.type) {
+                    case 'required':
+                        passed = (inputRef.val.trim() !== '')
+                        break;
+                    case 'email':
+                        passed = emailReg.test(inputRef.val)
+                        break;
+                    default:
+                        break;
+                }
+                return passed
+            })
+            inputRef.error = !allPassed
+        }
+    }
+
+    return {
+        inputRef,
+        updateValue,
+        validateInput,
+        
+    }
+  }
+});
+</script>
+
+<style scoped lang="less">
+  
+</style>
